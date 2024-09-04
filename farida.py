693e269c815d7f80412e7f318744920267186b03
mrinsaf <mr.insaf@list.ru>
2024-09-04 19:16:15 +0300

    переименовал main.py в farida.py

--- farida.py
+++ farida.py
@@ -559,7 +559,7 @@ async def main(token: str) -> None:
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python main.py <token>")
+        print("Usage: python farida.py <token>")
     else:
         try:
             TOKEN = sys.argv[1]
