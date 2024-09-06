863cfb353b8349b3259fcc7a68a2968ffc79b4aa
mrinsaf <mr.insaf@list.ru>
2024-09-06 12:05:24 +0300

    Создал пул соединений для бд

--- db.py
+++ db.py
@@ -3,65 +3,82 @@ from datetime import datetime
 from models import Student
 
 import mysql
-from mysql.connector import Error
+from mysql.connector import Error, pooling
 
 # Параметры подключения к базе данных MySQL
 config = {
     'user': 'stepan',
     'password': 'stepan',
     'host': '185.50.202.243',
-    'database': 'farida'
+    'database': 'farida',
 }
 
+pool = pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=5,
+    **config
+)
 
-# Создаем подключение
-def create_connection():
+
+def get_connection():
     try:
-        connection = mysql.connector.connect(**config)
+        connection = pool.get_connection()
         if connection.is_connected():
             print("Успешное подключение к базе данных")
             return connection
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Ошибка подключения: {e}")
         return None
 
 
-# Устанавливаем соединение и создаем курсор
-conn = create_connection()
-cursor = conn.cursor()
-
-
 def select(query):
-    conn = sqlite3.connect('grades.db')
-    query = query
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    conn.commit()
-    return rows
+    conn = get_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            conn.commit()
+            return rows
+        finally:
+            cursor.close()
+            conn.close()  # Возвращает соединение в пул
+    else:
+        return None
 
 
 def insert(table_name: str, data_list: list, auto_increment_id: int = 1):
     try:
         print("Start insert")
 
-        # Получаем информацию о столбцах в таблице
-        cursor.execute(f"SHOW COLUMNS FROM {table_name}")
-        columns = [column[0] for column in cursor.fetchall()]
-        columns = columns[auto_increment_id:]  # Убираем auto_increment, если нужно
-        print(columns)
+        # Получаем соединение из пула
+        conn = get_connection()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"SHOW COLUMNS FROM {table_name}")
+                columns = [column[0] for column in cursor.fetchall()]
+                columns = columns[auto_increment_id:]  # Убираем auto_increment, если нужно
+                print(columns)
 
-        # Генерируем SQL-запрос для вставки данных
-        placeholders = ', '.join(['%s'] * len(columns))
-        query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({placeholders})"
+                placeholders = ', '.join(['%s'] * len(columns))
+                query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({placeholders})"
 
-        # Вставляем данные в таблицу
-        cursor.execute(query, data_list)
-        row_id = cursor.lastrowid
-        conn.commit()
-        print("Finished insert")
-        return row_id
+                cursor.execute(query, data_list)
+                row_id = cursor.lastrowid
+                conn.commit()
+                print("Finished insert")
+                return row_id
+            except Exception as e:
+                print(f"Исключение при insert: {e}")
+            finally:
+                cursor.close()
+                conn.close()  # Возвращает соединение в пул
+        else:
+            return None
     except Exception as e:
-        print(f"Исключение при insert: {e}")
+        print(f"Исключение при получении соединения: {e}")
+        return None
 
 
 def find_student_by_surname_and_group_id(surname: str, group_id: str):
@@ -282,12 +299,41 @@ def get_hashed_password(teacher_id: int):
 def authorise_teacher(teacher_id: int):
     current_datetime = datetime.now()
     formatted_datetime = current_datetime.strftime("%d.%m.%Y %H:%M:%S")
-    cursor.execute('UPDATE teachers SET is_authorised = 1, auth_dttm = ? WHERE teacher_id = ?',
-                   (formatted_datetime, teacher_id))
 
-    # Применяем изменения
-    conn.commit()
+    conn = get_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(
+                'UPDATE teachers SET is_authorised = 1, auth_dttm = %s WHERE teacher_id = %s',
+                (formatted_datetime, teacher_id)
+            )
+            conn.commit()
+            print(f"Teacher with ID {teacher_id} authorised.")
+        except Exception as e:
+            print(f"Исключение при авторизации учителя: {e}")
+        finally:
+            cursor.close()
+            conn.close()  # Возвращает соединение в пул
+    else:
+        print("Не удалось получить соединение.")
 
 
 def unauthorise_teacher(teacher_id: int):
-    cursor.execute(f'UPDATE teachers SET is_authorised = 0 WHERE teacher_id = {teacher_id}')
+    conn = get_connection()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(
+                'UPDATE teachers SET is_authorised = 0 WHERE teacher_id = %s',
+                (teacher_id,)
+            )
+            conn.commit()
+            print(f"Teacher with ID {teacher_id} unauthorised.")
+        except Exception as e:
+            print(f"Исключение при деавторизации учителя: {e}")
+        finally:
+            cursor.close()
+            conn.close()  # Возвращает соединение в пул
+    else:
+        print("Не удалось получить соединение.")
